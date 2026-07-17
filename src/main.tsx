5703f36a1a3e922c8799a58382c8ede9b59bd75c
eslamshyba220-debug <eslamshyba220@gmail.com>
2026-07-17 15:39:03 +0300

    Add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import './index.css';
 
 // Register PWA Service Worker
@@ -13,5 +15,7 @@ if ('serviceWorker' in navigator) {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 );
